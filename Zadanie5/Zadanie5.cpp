04f72571a10f00b5808d7035ad6e3b57ba5634ed
IlonaTopolD3 <ilona.topol@gmail.com>
2022-06-15 16:19:14 +0200

    utworzenie struktur

--- Zadanie5/Zadanie5.cpp
+++ Zadanie5/Zadanie5.cpp
@@ -4,7 +4,33 @@
 #include <iostream>
 
 using namespace std;
-int main()
+
+struct BMP_header
 {
-    cout<<"Stworzenie repozytorium do zadania 5";
+    char name[2];
+    unsigned int size;
+    unsigned short int reserved_1;
+    unsigned short int reserved_2;
+    unsigned int offset;
+}bmp_header;
+
+struct DIB_header {
+    unsigned int headerSize;
+    unsigned int width;
+    unsigned int height;
+    unsigned short int planes;
+    unsigned short int bitPerPixel;
+    unsigned int compresion;
+    unsigned int imageSize;
+    unsigned int xPelsPerMeter;
+    unsigned int yPelsPerMeter;
+    unsigned int colorUsed;
+    unsigned int colorImportant;
+
+}dib_header;
+
+
+int main(int argc, char* argv[])
+{
+   return 0;
 }
